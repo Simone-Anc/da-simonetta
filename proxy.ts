04c1b491ba4dfe1b6dbff579056da8789028a2a9
Simone-Anc <simone.ancillai@integrasistemi.it>
2026-04-19 16:13:34 +0200

    fix: escludi gestione-menu-ds95 dal middleware

--- proxy.ts
+++ proxy.ts
@@ -4,5 +4,6 @@ import { routing } from './routing'
 export default createMiddleware(routing)
 
 export const config = {
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
+  // Esclude gestione-menu-ds95 dal routing multilingua
+  matcher: ['/((?!api|_next|_vercel|gestione-menu-ds95|.*\\..*).*)'],
 }
